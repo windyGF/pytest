e0106f3e6269424dee6f0227700eea71545d40a0
windyGF <844275259@qq.com>
2019-11-18 22:46:14 +0800

    add price.py

---
+++ learn_1118/price.py
@@ -0,0 +1,23 @@
+#价目表
+width = int(input("Please enter width:"))
+
+price_width = 10
+item_width = width - price_width
+
+header_fmt = '{{:{}}}{{:>{}}}'.format(item_width,price_width)
+fmt = '{{:{}}}{{:>{}.2f}}'.format(item_width,price_width)
+
+print('='*width)
+
+print(header_fmt.format('Item','Price'))
+
+print('-'*width)
+
+print(fmt.format('Apple',0.4))
+print(fmt.format('Apple',0.4))
+print(fmt.format('Apple',0.4))
+print(fmt.format('Apple',0.4))
+print(fmt.format('Apple',0.4))
+print(fmt.format('Apple',0.4))
+
+print('='*width)
